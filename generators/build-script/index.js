e3259ddf89d04fed40652223debf97d43d4bdf17
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2016-06-20 02:35:35 -0300

    updated build generator to include a '--recompile' flag (creates just code-based objects)

--- generators/build-script/index.js
+++ generators/build-script/index.js
@@ -1,17 +1,33 @@
 var generators = require('yeoman-generator');
 var _ = require('lodash');
 var pathExists = require('path-exists');
-var readDir = require('read-dir');
 var readFile = require('read-file');
 var chmod = require('chmod');
+var glob = require('glob');
 
 module.exports = generators.Base.extend({
 
+  constructor: function() {
+    generators.Base.apply(this, arguments);
+
+    this.option('recompile');
+  },
+
   writing: function() {
     var template = _.template(
-      this.fs.read(this.templatePath('build_script.ejs'))
+      this.fs.read(this.templatePath(
+        this.options.recompile ? 'recompile_script.ejs' : 'build_script.ejs'
+      ))
     );
-    var buildscriptfile = this.config.get('dbname') + '/build.sql';
+    var buildscriptfile = this.config.get('dbname') +
+      (this.options.recompile ? '/recompile.sql' : '/build.sql');
+    var bashscriptfile = this.config.get('dbname') +
+      (this.options.recompile ? '/recompile.sh' : '/build.sh');
+    var runnertemplate = _.template(
+      this.fs.read(this.templatePath(
+        this.options.recompile ? 'recompile_runner.ejs' : 'build_runner.ejs'
+      ))
+    );
 
     var build = {};
 
@@ -21,37 +37,47 @@ module.exports = generators.Base.extend({
 
     var path = {
       db: this.destinationPath(build.dbname),
-      schemas: this.destinationPath(build.dbname + '/schemas'),
-      tables: this.destinationPath(build.dbname + '/tables')
+      schemas: this.destinationPath(build.dbname + '/schemas/'),
+      tables: this.destinationPath(build.dbname + '/tables/'),
+      functions: this.destinationPath(build.dbname + '/functions/'),
+      views: this.destinationPath(build.dbname + '/views/')
     };
 
     build.databasescript = readFile.sync(path.db + '/db.sql').toString();
 
     build.useschemas = pathExists.sync(path.schemas);
     build.usetables = pathExists.sync(path.tables);
+    build.usefunctions = pathExists.sync(path.functions);
+    build.useviews = pathExists.sync(path.views);
 
-    var schemafiles = build.useschemas ? readDir(path.schemas) : [];
-    var tablefiles = build.usetables ? readDir(path.tables) : [];
+    var schemafiles = build.useschemas ? glob.sync(path.schemas + '**/*.sql') : [];
+    var tablefiles = build.usetables ? glob.sync(path.tables + '**/*.sql') : [];
+    var functionfiles = build.usefunctions ? glob.sync(path.functions + '**/*.sql') : [];
+    var viewfiles = build.useviews ? glob.sync(path.views + '**/*.sql') : [];
 
     build.schemascripts = [];
     build.tablescripts = [];
+    build.functionscripts = [];
+    build.viewscripts = [];
 
     var builder = function(collection) {
-      return function(sql, name) {
-        build[collection].push({ name: name, sql: sql });
+      return function(name) {
+        build[collection].push({ name: name, sql: readFile.sync(name).toString() });
       };
     };
 
     _.forEach(schemafiles, builder('schemascripts'));
     _.forEach(tablefiles, builder('tablescripts'));
+    _.forEach(functionfiles, builder('functionscripts'));
+    _.forEach(viewfiles, builder('viewscripts'));
 
     this.fs.write(this.destinationPath(buildscriptfile), template(build));
+    this.fs.write(this.destinationPath(bashscriptfile), runnertemplate(build));
 
-    var bashscriptfile = this.config.get('dbname') + '/build.sh';
+    this.bashscriptfile = this.destinationPath(bashscriptfile);
+  },
 
-    template = _.template(this.fs.read(this.templatePath('build_runner.ejs')));
-
-    this.fs.write(this.destinationPath(bashscriptfile), template(build));
-    chmod(this.destinationPath(bashscriptfile), { execute: true });
+  end: function() {
+    chmod(this.bashscriptfile, { execute: true });
   }
 })
