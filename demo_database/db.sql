0c47c955bb18eb69aabdc824ff7b28d9c42191be
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2016-06-20 15:03:29 -0300

    fixed wrongful syntax on demo_database's db.sql

--- demo_database/db.sql
+++ demo_database/db.sql
@@ -1,3 +1,3 @@
-CREATE USER IF NOT EXISTS dbo_demo_database WITH PASSWORD 'demodemo';
+CREATE USER dbo_demo_database WITH PASSWORD 'demodemo';
 
-CREATE DATABASE IF NOT EXISTS demo_database WITH OWNER = dbo_demo_database;
+CREATE DATABASE demo_database WITH OWNER = dbo_demo_database;
